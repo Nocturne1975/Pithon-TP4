521841b7c519b938dcaaeb174f162056077d2777
Nocturne1975 <soniacorbin4@gmail.com>
2025-07-10 08:10:34 -0400

    Finalisation avant fusion

--- tp4-code/2025H-420-910-MA-Pithon/run_tests.ps1
+++
@@ -1,7 +0,0 @@
-$env:PYTHONPATH="$PSScriptRoot\src"
-pytest -q --tb=short
-if ($LASTEXITCODE -ne 0) {
-    Write-Host "❌ Des tests ont échoué." -ForegroundColor Red
-} else {
-    Write-Host "✅ Tous les tests ont réussi !" -ForegroundColor Green
-}
